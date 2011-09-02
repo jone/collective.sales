e19938cee3a745cb14cac9d00830590face67840
Jonas Baumann <jone@jone.ch>
2011-09-02 15:28:28 +0200

    shop category: added some more tests

--- src/collective.sales/collective/sales/tests/test_shopcategory.py
+++ src/collective.sales/collective/sales/tests/test_shopcategory.py
@@ -1,11 +1,28 @@
+from collective.sales.shopcategory import IShopCategory
 from collective.sales.testing import SALES_INTEGRATION_TESTING
 from collective.sales.tests.helpers import RoleSwitcher
+from plone.dexterity.interfaces import IDexterityFTI
+from zope.component import createObject
+from zope.component import queryUtility
 import unittest2 as unittest
 
 
 class TestShopCategory(unittest.TestCase):
 
     layer = SALES_INTEGRATION_TESTING
+
+    def test_fti(self):
+        fti = queryUtility(IDexterityFTI,
+                           name='collective.sales.shopcategory')
+        self.assertNotEquals(None, fti)
+
+    def test_factory(self):
+        fti = queryUtility(IDexterityFTI,
+                           name='collective.sales.shopcategory')
+
+        factory = fti.factory
+        new_object = createObject(factory)
+        self.failUnless(IShopCategory.providedBy(new_object))
 
     def test_category_creatable(self):
         portal = self.layer['portal']
